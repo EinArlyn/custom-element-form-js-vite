176fe938b3915e6eca42b22d714e1c24d0b5fec9
Aleksandr Myroshnik <GreshnikAlexM@gmail.com>
2024-04-23 16:21:15 +0300

    Changed implementation of connecting to @bpmn.io/form.js through its own library

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script lang="ts">
     import { defineComponent, ref, onMounted } from 'vue';
     //@ts-ignore
-    import FormEditor from '@einarlyn/custom-form-editor'
+    import { FormEditor } from '@einarlyn/bpmn-form-extended';
+    import '@einarlyn/bpmn-form-extended/dist/assets/styles.css';
 
     export default defineComponent({
         name: 'App',
@@ -20,6 +21,11 @@
             onMounted(async () => {
                 formEditorRef.value = new FormEditor({
                     container: document.querySelector('#form-editor'),
+                });
+
+                formEditorRef.value.on('change', (schema: any) => {
+                    console.log('schema', schema);
+                    schema.value = schema;
                 });
                 formEditorRef.value.importSchema(schema.value);
             });
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  optimizeDeps: {
-    include: ['@einarlyn/custom-form-editor']
-  }
-  // resolve: {
-  //   alias: {
-  //     'preact/hooks': path.resolve(__dirname, 'node_modules/preact/hooks/dist/hooks.module.js'),
-  //     'preact/jsx-runtime': path.resolve(__dirname, 'node_modules/preact/jsx-runtime/dist/jsxRuntime.module.js'),
-  //     'preact/compat': path.resolve(__dirname, 'node_modules/preact/compat/dist/compat.module.js'),
-  //     'preact': path.resolve(__dirname, 'node_modules/preact/dist/preact.module.js'),
-  //     '../preact/hooks': path.resolve(__dirname, 'node_modules/preact/hooks/dist/hooks.module.js'),
-  //     '../preact/jsx-runtime': path.resolve(__dirname, 'node_modules/preact/jsx-runtime/dist/jsxRuntime.module.js'),
-  //     '../preact': path.resolve(__dirname, 'node_modules/preact/dist/preact.module.js')
-  //   },
-  // },
 })
