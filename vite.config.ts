60222e364142691e27b4e9e420476645dca6226e
Aleksandr Myroshnik <GreshnikAlexM@gmail.com>
2024-06-18 17:17:07 +0300

    chore: Update npm dependencies and optimize build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+// @ts-ignore
+import { terser } from 'rollup-plugin-terser';
 
 export default defineConfig({
   plugins: [vue()],
   optimizeDeps: {
-    include: ['classnames'],
-    exclude: ['@einarlyn/bpmn-form-extended'],
+    exclude: [
+      '@einarlyn/bpmn-form-extended',
+      '@bpmn-io/properties-panel',
+      '@bpmn-io/form-js',
+    ],
   },
   resolve: {
     alias: {
@@ -13,6 +18,67 @@ export default defineConfig({
     },
   },
   build: {
-    minify: false,
-  },
+    rollupOptions: {
+      plugins: [terser({
+        compress: {
+          drop_console: false,
+          pure_funcs: ['RangeField', 'formFields.register'],
+        },
+        mangle: {
+          reserved: ['RangeField', 'formFields'],
+          keep_classnames: true,
+          keep_fnames: true,
+        },
+        format: {
+          comments: false,
+        },
+        module: true,
+      })],
+      external: [
+        '@einarlyn/bpmn-form-extended',
+        '@bpmn-io/properties-panel',
+        '@bpmn-io/form-js',
+      ],
+      output: {
+        globals: {
+          '@einarlyn/bpmn-form-extended': '@einarlyn/bpmn-form-extended',
+          '@bpmn-io/properties-panel': '@bpmn-io/properties-panel',
+          '@bpmn-io/form-js': '@bpmn-io/form-js',
+        },
+        manualChunks(id) {
+          if (
+            id.includes('@einarlyn/bpmn-form-extended') ||
+            id.includes('@bpmn-io/properties-panel') ||
+            id.includes('@bpmn-io/form-js')
+          ) {
+            console.log('id', id);
+            return 'no-minify';
+          }
+        },
+      },
+    },
+    commonjsOptions: {
+      include: [/node_modules/],
+      exclude: [
+        '@einarlyn/bpmn-form-extended',
+        '@bpmn-io/properties-panel',
+        '@bpmn-io/form-js',
+      ],
+      transformMixedEsModules: true,
+    },
+    terserOptions: {
+      mangle: {
+        keep_classnames: true,
+        keep_fnames: true,
+        reserved: ['RangeField', 'formFields'],
+      },
+      compress: {
+        drop_console: false,
+        pure_funcs: ['RangeField', 'formFields.register'],
+      },
+      format: {
+        comments: false,
+      },
+    },
+  }
 });
